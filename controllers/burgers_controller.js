565e329e3233e05586f2197c98bea308c08ca52e
BakerShoeMaker <info@quackenworth.com>
2017-11-25 16:35:35 -0800

    The routing and handlebars template is working.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 var burgers = require("../models/burger.js");//import model to use database function
 
-router.get("/", function(req, rest){
-    app.send("You made it to the index page!");
-
+router.get("/", function(req, res){
+    //res.send("You made it to the index page!");
+    console.log("Yes this route is working!!!!!");
+    res.render('index');
 });
 
 module.exports = router;
